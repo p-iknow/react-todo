67389aa5dbef7539d49ecf5b58d847580ee6ee83
P-iknow <apricotsoul@gmail.com>
2019-08-07 22:20:50 +0900

    config: async 활용을 위해 babel runtime plugin 설치

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,15 @@
+/* eslint-disable func-names */
 // babel.config.js
 module.exports = function(api) {
   api.cache(true);
 
   const presets = [['@babel/preset-env'], ['@babel/preset-react']];
 
-  const plugins = ['react-hot-loader/babel'];
+  const plugins = [
+    'react-hot-loader/babel',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime'
+  ];
 
   return {
     presets,
