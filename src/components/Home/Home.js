897b0b713d70b13e8d1dd2f7b3809bd715195dcb
P-iknow <apricotsoul@gmail.com>
2019-09-15 22:26:45 +0900

    style: Home, Nav

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useTodoState } from '../TodoContext';
+import './Home.scss';
 import { countTodoStatus } from '../../utils';
 
 const Home = () => {
   const todos = useTodoState();
   const { todo, done } = countTodoStatus(todos);
   return (
-    <>
+    <section className="home-wrapper">
       <h2>
         반갑습니다.
         <br />
         할일 관리 어플리케이션 입니다.
       </h2>
       <p>
-        현재 해야할 일이 <span>{todo}</span>개, <br />
-        완료된 일이 <span>{done}</span>개 있습니다.
+        현재 해야할 일이 <span className="point">{todo}</span>개, <br />
+        완료된 일이 <span className="point">{done}</span>개 있습니다.
       </p>
-    </>
+    </section>
   );
 };
 
